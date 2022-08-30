0768e358a95dc44de1517bbabbfd3ccaf8b40a33
useDuanly <use.d.v.a.dev@gmail.com>
2022-08-30 14:24:48 +0100

    get app

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,16 @@
-import { Header } from "./Header.js"
-import { Footer } from "./Footer.js"
+import { Header } from "./Header.js";
+import { Footer } from "./Footer.js";
 
-export function Layout ({ children }) {
+export function Layout({ children, title, footer = true, description }) {
   return (
     <>
+      <head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </head>
       <Header />
-
-      <main className='max-w-xl m-auto'>
-        {children}
-      </main>
-
-      <Footer />
+      <main className="max-w-xl m-auto">{children}</main>
+      {footer && <Footer />}
     </>
-  )
+  );
 }
